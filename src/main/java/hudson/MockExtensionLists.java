90e526871b21edadf45f91267a1e48e2c510d91d
Vihaan Thora <77713537+vihaanthora@users.noreply.github.com>
2022-07-06 21:58:57 +0530

    Update and release pipeline-metadata-utils (#1)
    The plugin manager used by the pipeline steps generator can be published as an artifact. This will help other tools that want to use the power of the plugin manager without requiring to start a separate Jenkins instance.
    
    Co-authored-by: Mark Waite <mark.earl.waite@gmail.com>

--- src/main/java/hudson/MockExtensionLists.java
+++ src/main/java/hudson/MockExtensionLists.java
@@ -1,44 +1,57 @@
 package hudson;
 
-import java.util.List;
-import java.util.HashMap;
-import java.util.Map;
-import org.jenkinsci.infra.tools.HyperLocalPluginManager;
+import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.spy;
 
-import static org.mockito.Mockito.*;
-import org.mockito.stubbing.Answer;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.jenkinsci.infra.tools.HyperLocalPluginManager;
 import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
+
 import hudson.model.Hudson;
+import hudson.model.listeners.SaveableListener;
 import jenkins.model.Jenkins;
 
 /**
  * A mocked way to get at {@link ExtensionList}s. In {@code hudson} package due to protected access in {@link ExtensionList}.
  */
-public class MockExtensionLists {
-    private static Map<String, ExtensionList> extensionLists = new HashMap<String, ExtensionList>();
 
-    public ExtensionList getMockExtensionList(HyperLocalPluginManager hlpm, Jenkins hudson, Class type) {
+public class MockExtensionLists {
+    private static Map<String, ExtensionList<?>> extensionLists = new HashMap<String, ExtensionList<?>>();
+
+    public ExtensionList<?> getMockExtensionList(HyperLocalPluginManager hlpm, Jenkins hudson, Class<?> type) {
+        if (SaveableListener.class.equals(type)) {
+            ExtensionList<?> ret = mock(ExtensionList.class);
+            doReturn(Collections.emptyIterator()).when(ret).iterator();
+            return ret;
+        }
         if(extensionLists.get(type.getName()) != null) {
             return extensionLists.get(type.getName());
         } else {
-            MockExtensionList mockList = new MockExtensionList(hlpm, hudson, type);
+            MockExtensionList<?> mockList = new MockExtensionList<>(hlpm, hudson, type);
             extensionLists.put(type.getName(), mockList.getMockExtensionList());
             return mockList.getMockExtensionList();
         }
     }
 
-    private static class MockExtensionList {
-        ExtensionList mockExtensionList;
+    private class MockExtensionList<T> {
+        ExtensionList<T> mockExtensionList;
 
-        public MockExtensionList(HyperLocalPluginManager hlpm, Jenkins hudson, Class type) {
-            ExtensionList realList = ExtensionList.create(hudson, type);
+        public MockExtensionList(HyperLocalPluginManager hlpm, Jenkins hudson, Class<T> type) {
+            ExtensionList<T> realList = ExtensionList.create(hudson, type);
             mockExtensionList = spy(realList);
 
             doReturn("Locking resources").when(mockExtensionList).getLoadLock();
             doAnswer(mockLoad(hlpm)).when(mockExtensionList).load();
         }
 
-        private <T> Answer<List<ExtensionComponent<T>>> mockLoad(HyperLocalPluginManager hlpm) {
+        private Answer<List<ExtensionComponent<T>>> mockLoad(HyperLocalPluginManager hlpm) {
             return new Answer<List<ExtensionComponent<T>>>() {
                 public List<ExtensionComponent<T>> answer(InvocationOnMock invocation) throws Throwable {
                     return hlpm.getPluginStrategy().findComponents(mockExtensionList.extensionType, (Hudson)null);
@@ -46,7 +59,7 @@ public class MockExtensionLists {
             };
         }
 
-        public ExtensionList getMockExtensionList(){
+        public ExtensionList<T> getMockExtensionList(){
             return mockExtensionList;
         }
     }
--- src/main/java/hudson/MockJenkins.java
+++ src/main/java/hudson/MockJenkins.java
@@ -1,14 +1,3 @@
-package hudson;
-
-import hudson.init.InitMilestone;
-import hudson.model.Hudson; //only needed until Hudson reference is removed from the ExtensionList.
-import jenkins.model.Jenkins;
-import org.jenkinsci.infra.tools.HyperLocalPluginManager;
-
-import static org.mockito.Mockito.*;
-import org.mockito.invocation.InvocationOnMock;
-import org.mockito.stubbing.Answer;
-
 /**
  * Mock for the Jenkins/Hudson object required for processing
  * the various steps.  This became very important after introducing
@@ -22,42 +11,86 @@ import org.mockito.stubbing.Answer;
  * desired class, it has to be of Hudson.  This should be changed
  * when ExtensionList no longer requires a Hudon object.
  */
+package hudson;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.io.File;
+import java.lang.reflect.Field;
+
+import javax.servlet.ServletContext;
+
+import org.jenkinsci.infra.tools.HyperLocalPluginManager;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
+
+import hudson.init.InitMilestone;
+import hudson.model.Computer;
+import hudson.model.Descriptor;
+import hudson.model.Hudson; //only needed until Hudson reference is removed from the ExtensionList.
+import jenkins.install.InstallState;
+import jenkins.model.Jenkins;
+
 public class MockJenkins {
-    private MockExtensionLists mockLookup = new MockExtensionLists();
+     private MockExtensionLists mockLookup = new MockExtensionLists();
 
     /**
      * There are a few methods that need to be mocked in order for setup to work properly:
-     *     * getPluginManager -&gt; must return HyperLocalPluginManager
-     *     * getInitLevel     -&gt; COMPLETED; Jenkins is "setup" as soon as the pm is populated
-     *     * getExtensionList -&gt; use the MockExtensionLists
-     *     * getPlugin        -&gt; get the Plugin information from HyperLocalPluginManager
-     *
-     * @param pm plugin manager to be used in the mock Jenkins controller
-     * @return mock Jenkins object with pm as the plugin manager
+     *     * getPluginManager -> must return HyperLocalPluginManager
+     *     * getInitLevel     -> COMPLETED; Jenkins is "setup" as soon as the pm is populated
+     *     * getExtensionList -> use the MockExtensionLists
+     *     * getPlugin        -> get the Plugin information from HyperLocalPluginManager
      */
-    public Jenkins getMockJenkins(HyperLocalPluginManager pm) {
-        Jenkins mockJenkins = mock(Hudson.class); //required by ExtensionList
-        when(mockJenkins.getPluginManager()).thenReturn(pm);
-        when(mockJenkins.getInitLevel()).thenReturn(InitMilestone.COMPLETED);
-
-        doAnswer(new Answer<ExtensionList>() {
+     @SuppressWarnings({"unchecked", "rawtypes"})
+     public Jenkins getMockJenkins(HyperLocalPluginManager pm) {
+         Jenkins mockJenkins = mock(Hudson.class); //required by ExtensionList
+         when(mockJenkins.getPluginManager()).thenReturn(pm);
+         when(mockJenkins.getInitLevel()).thenReturn(InitMilestone.COMPLETED);
+         when(mockJenkins.getInstallState()).thenReturn(InstallState.TEST);
+         when(mockJenkins.getComputers()).thenReturn(new Computer[0]);
+         when(mockJenkins.getRootDir()).thenReturn(new File(System.getProperty("java.io.tmpdir")));
+         try {
+             Field lookup = mockJenkins.getClass().getField("lookup");
+             lookup.setAccessible(true);
+             lookup.set(mockJenkins, new Lookup());
+             Field servletContext = mockJenkins.getClass().getField("servletContext");
+             servletContext.setAccessible(true);
+             servletContext.set(mockJenkins, mock(ServletContext.class));
+         } catch (NoSuchFieldException | IllegalAccessException e) {
+             e.printStackTrace();
+         }
+         doAnswer(new Answer<ExtensionList>() {
             @Override
             public ExtensionList answer(InvocationOnMock invocation) throws Throwable {
                 Object[] args = invocation.getArguments();
                 return mockLookup.getMockExtensionList(pm, mockJenkins, (Class) args[0]);
             }
-        }).when(mockJenkins).getExtensionList(any(Class.class));
+         }).when(mockJenkins).getExtensionList(any(Class.class));
 
-        doAnswer(new Answer<Plugin>() {
-            @Override
-            public Plugin answer(InvocationOnMock invocation) throws Throwable {
-                Object[] args = invocation.getArguments();
-                PluginWrapper p = pm.getPlugin((Class) args[0]);
-                if(p==null)     return null; //not actually loaded; might need an override
-                return p.getPlugin();
-            }
-        }).when(mockJenkins).getPlugin(any(Class.class));
+         doAnswer(invocation -> {
+             Object[] args = invocation.getArguments();
+             for (Object _d : mockLookup.getMockExtensionList(pm, mockJenkins, Descriptor.class)) {
+                 Descriptor d = (Descriptor) _d;
+                 if (d.clazz == args[0]) {
+                     return d;
+                 }
+             }
+             return null;
+         }).when(mockJenkins).getDescriptor(any(Class.class));
 
-        return mockJenkins;
-    }
-}
+                  doAnswer(new Answer<Plugin>() {
+             @Override
+             public Plugin answer(InvocationOnMock invocation) throws Throwable {
+                 Object[] args = invocation.getArguments();
+                 PluginWrapper p = pm.getPlugin((Class) args[0]);
+                 if(p==null)     return null; //not actually loaded; might need an override
+                 return p.getPlugin();
+             }
+         }).when(mockJenkins).getPlugin(any(Class.class));
+
+         return mockJenkins;
+     }
+ }
